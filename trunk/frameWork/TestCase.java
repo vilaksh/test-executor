2ff64db47f1396576d873c648417409e30969a64
viji133hp@gmail.com <viji133hp@gmail.com@1bd81bd7-cdeb-239f-4c92-c106464747c5>
2011-06-11 17:51:56 +0000

    Few fixes with ArrayList type

--- trunk/frameWork/TestCase.java
+++ trunk/frameWork/TestCase.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 public class TestCase 
 {
 	public String _id;
-	public ArrayList _input;
-	public ArrayList _expectedOutput;
-	public ArrayList _actualOutput;
+	public ArrayList<String> _input;
+	public ArrayList<String> _expectedOutput;
+	public ArrayList<String> _actualOutput;
 	public double _executionTime;
 	public boolean _execute;
 	
-	public TestCase(String id, ArrayList input, ArrayList expOutput)
+	public TestCase(String id, ArrayList<String> input, ArrayList<String> expOutput)
 	{
 		_id = id;
 		_input = input;
